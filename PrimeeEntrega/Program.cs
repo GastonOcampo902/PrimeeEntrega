327d74f238634dedba30e5b63c5ff46443d967d8
gaston_ocampo ocampo <gaston_ocampo22@hotmail.com>
2025-03-23 18:26:50 -0300

    Dos clases Abstractas

--- PrimeeEntrega/Program.cs
+++ PrimeeEntrega/Program.cs
@@ -7,6 +7,47 @@ namespace PrimeeEntrega
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+           
+        }
+    }
+    //Primer clase 
+    public class Guitarra
+    {
+       //Atributos
+        public string Marca;
+        public string Modelo;
+        public int Precio;
+        public int AñoFabicacion;
+
+        //Metodos
+        public void Sonido () 
+            {
+                Console.WriteLine("La guitarra esta sonando.");
+            }
+        public void Costo()
+        {
+            Console.WriteLine("La guitarra es barata.");
+        }
+     }
+
+    //Segunda clase
+    public class Perro
+    {
+        //Atributos
+        public string Color;
+        public string Nombre;
+        public string Años;
+
+        //Metodos
+
+        public void Comer()
+        {
+            Console.WriteLine("El perro esta comiendo.");
+        }
+        public void Jugar()
+        {
+            Console.WriteLine("El perro esta jugando.");
         }
     }
 }
